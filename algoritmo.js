034da5dec9696187c0923e9e89e2684ef2d60671
Roberto Alagemovits <robertomovits@gmail.com>
2023-03-28 10:12:09 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+document.querySelector("body").innerHTML = `
+<h1 style="color: blue;">lista de tareas pendientes</h1>
+<ol>
+    <li>Estudiar para mis pruebas aca y en Brasil</li>
+    <li>Responder mensajes en Whatsapp</li>
+    <li>Empacar mi equipaje para viajar</li>
+</ol>
+<ul>
+    <li class="img"><img src="https://picsum.photos/200" alt=""></li>
+    <li class="img"><img src="https://picsum.photos/200" alt=""></li>
+</ul>
+`
